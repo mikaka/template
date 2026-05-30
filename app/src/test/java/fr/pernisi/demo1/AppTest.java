7aefd0c6e3542a41934115516351e93c53b1ba7c
mikaka <petitane1@hotmail.com>
2026-05-30 17:39:14 +0200

    up

--- app/src/test/java/fr/pernisi/demo1/AppTest.java
+++ app/src/test/java/fr/pernisi/demo1/AppTest.java
@@ -1,16 +1,16 @@
 package fr.pernisi.demo1;
 
 
+import org.junit.jupiter.api.BeforeEach;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-
-{
+public class AppTest {
     /**
      * Rigourous Test :-)
      */
+    @BeforeEach
     public void testApp()
     {
 
